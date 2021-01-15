5a399d4a47b1f0d31cfc8171921f0a35a6f6add8
Joandysson Gama <joandysson.gama@gmail.com>
2021-01-15 16:42:01 -0300

    added new component and new functionality

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:weekly_finance_app/components/transaction_item.dart';
 import 'package:weekly_finance_app/models/transaction.dart';
-import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -37,46 +37,25 @@ class TransactionList extends StatelessWidget {
               );
             },
           )
-        : ListView.builder(
+        :
+        // ListView(
+        //     children: transactions.map((tr) {
+        //       return TransactionItem(
+        //         key: ValueKey(tr.id),
+        //         tr: tr,
+        //         onRemove: onRemove,
+        //       );
+        //     }).toList(),
+        //   );
+
+        ListView.builder(
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
               final tr = transactions[index];
-              return Card(
-                elevation: 5,
-                margin: EdgeInsets.symmetric(
-                  vertical: 8,
-                  horizontal: 5,
-                ),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    radius: 30,
-                    child: Padding(
-                      padding: const EdgeInsets.all(6),
-                      child: FittedBox(
-                        child: Text("R\$${tr.value}"),
-                      ),
-                    ),
-                  ),
-                  title: Text(
-                    tr.title,
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  subtitle: Text(
-                    DateFormat('dd MM y').format(tr.date),
-                  ),
-                  trailing: MediaQuery.of(context).size.width > 412
-                      ? FlatButton.icon(
-                          onPressed: () => onRemove(tr.id),
-                          icon: Icon(Icons.delete),
-                          label: Text('Excluir'),
-                          textColor: Theme.of(context).errorColor,
-                        )
-                      : IconButton(
-                          icon: Icon(Icons.delete),
-                          color: Theme.of(context).errorColor,
-                          onPressed: () => onRemove(tr.id),
-                        ),
-                ),
+              return TransactionItem(
+                key: GlobalObjectKey(tr.id),
+                tr: tr,
+                onRemove: onRemove,
               );
             },
           );
